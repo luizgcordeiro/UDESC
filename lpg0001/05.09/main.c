1b6c72d29e95bf40402d1f48d4af877c943a93d8
Luiz Cordeiro <luizgc6@gmail.com>
2022-05-14 16:57:16 -0300

    common update

--- lpg0001/05.09/main.c
+++ lpg0001/05.09/main.c
@@ -6,10 +6,11 @@
 #define K 1000//Ordem dos numeros aleatorios a serem criados
 
 int maximo(int m, int n);
-int numero_de_elementos(int m);
+int numero_de_digitos(int m);
 int pot(int m,int k);
 void imprimir_matriz(int * a,int m, int n);
-void transposicao(int x[M][N], int y[N][M]);
+void transposicao(int * x, int * y, int m, int n);
+float media_pares(int *x ,int m,int n);
 
 int main() {
 
@@ -31,9 +32,11 @@ int main() {
   printf("\n");
   printf("A transposta eh\n");
   int t[N][M];
-  transposicao(m,t);
+  transposicao(*m,*t,M,N);
   imprimir_matriz(*t,N,M);
 
+  printf("\nA media dos valores pares eh ");
+  printf("%f\n",media_pares(*m,M,N));
   return 0;
 }
 
@@ -47,7 +50,7 @@ void imprimir_matriz(int * a, int m, int n) {
     }
   }
 
-  int k=numero_de_elementos(maiorelemento);//Tem espaço para sinal.
+  int k=numero_de_digitos(maiorelemento);//Tem espaço para sinal.
   for (i=0;i<M;i++) {
     for (j=0;j<N;j++) {
       printf("%*d ",k,a[m*i+j]);
@@ -70,7 +73,7 @@ int pot(int m,int k) {
   return m*pot(m,k-1);
 }
 
-int numero_de_elementos(int m) {
+int numero_de_digitos(int m) {
   int i=0;
   while (pot(10,i)<m) {
     /*Se sai em i=1, tem 1 dígito; se sai em i=2, tem 2...*/
@@ -80,13 +83,32 @@ int numero_de_elementos(int m) {
 }
 
 
-void transposicao(int x[M][N], int y[N][M]) {
+void transposicao(int * x, int * y, int m, int n) {
   /*Transpõe a matriz em x e grava o resultado em y*/
 
   int i,j;
-  for (i=0;i<M;i++) {
-    for (j=0;j<N;j++) {
-      y[j][i]=x[i][j];
+  for (i=0;i<m;i++) {
+    for (j=0;j<n;j++) {
+      y[n*j+i]=x[m*i+j];
     }
   }
+}
+
+float media_pares(int *x ,int m,int n) {
+  int i,j,soma=0,cont=0;
+
+  for (i=0;i<m;i++) {
+    for (j=0;j<n;j++) {
+      if (x[m*i+j]%2==0) {
+        cont++;
+        soma+=x[m*i+j];
+      }
+    }
+  }
+  if (cont==0) {
+    printf("Erro: sem numeros pares.");
+    return (float)0;
+  }
+
+  return (float)soma/cont;
 }
