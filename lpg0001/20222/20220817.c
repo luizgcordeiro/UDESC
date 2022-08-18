23e440e34a18cb9b3d174128538d1f13e3dd9338
Luiz Cordeiro <luizgc6@gmail.com>
2022-08-18 07:39:12 -0300

    small updates

--- lpg0001/20222/20220817.c
+++ lpg0001/20222/20220817.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//include <stdarg.h> // function arguments
+//#include <string.h>
 
-srand(time(NULL));
+void * _memcpy ( void * destination , void * source , size_t size ) {
+    for ( int i=0 ; i<size ; i++ ) {
+        ((char*)destination)[i]=((char*)source)[i];
+    }
+    return destination;
+}
+
+void swap ( void * A, void * B , size_t size) {
+    //Swaps whatever is being pointed to by A and B, n being the size of the data in bytes.
+    unsigned char temp[size];
+    memcpy(temp,A,size);
+    memcpy(A,B,size);
+    memcpy(B,temp,size);
+};
+
+int max_int( int a , int b) {
+    return (a>b) ? a : b;
+}
 
 int main ( int argc , char ** argv ) {
-    printf("%8.3f",3.1415926535);
-    printf("\n");
-    printf("%5.3f",12356873.1415926535);
+    int A=atoi(argv[1]);
+    int B=atoi(argv[2]);
+    printf("Os argumentos foram %d e %d.\n",A,B);
+
+    swap(&A,&B,sizeof(int));
+
+    printf("Em ordem trocada eles sao %d e %d.\n",A,B);
 
     return 0;
 }
