a8b4bb5ebaa5113d67c6fddaffca49a7ff1292c9
Luiz Cordeiro <luizgc6@gmail.com>
2022-08-29 12:35:24 -0300

    hoje

--- teste.c
+++ teste.c
@@ -12,9 +12,62 @@ long max_or(long a, long b) {
     return b;
 }
 
-int main(int argc, char **argv) {
-    long a=strtol(argv[1],NULL,10);
-    long b=strtol(argv[2],NULL,10);
-    printf("%d",max(a,b));
+int strlen_ ( char * st ) {
+    int i=0;
+    while (st[i]!=0) i++;
+    return i;
+}
+
+#include <stdio.h>
+ 
+int case_ ( char c) {
+    //0 minusculo; 1 maiusculo
+    if (c<='Z') {
+        return 1;
+    }
+    return 0;
+}
+
+void maiusculo (char *c) {
+    if (case_(*c)==0) {
+        *c += 'A'-'a';
+        return;
+    }
+    return;
+}
+
+void minusculo (char *c) {
+    if (case_(*c)==1) {
+        *c +='a'-'A';
+        return;
+    }
+    return;
+}
+
+int main() {
+ 
+    int N,i,look_for_char=1;
+    char c;
+    scanf("%d ",&N);
+    
+    for (i=0;i<N;i++) {
+        while((c=getchar())!=EOF && c!='\n') {
+            if (look_for_char) {
+                if (c!=' ') {
+                    printf("%c",c);
+                    look_for_char=0;
+                }
+            } else {
+                if (c==' ') {
+                    look_for_char=1;
+                }
+            }
+        }
+        
+        if (c=='\n') {
+            printf("\n");
+        }
+    }
+ 
     return 0;
 }
